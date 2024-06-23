ff838c2255497429cae0075ab4c73806122a555d
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-06-22 23:19:58 -0700

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,11 @@
+"use strict";
+// Question 1
+console.log("Albert Einstein once said, A person who never made a mistake never tried anything new.");
+// Question 2
+let famous_person = "Albert Einstein";
+let msg = `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+console.log(msg);
+// Question 3
+let myName = "\t\n Tahirah Roohi \t\n";
+console.log(myName);
+console.log(myName.trim());
